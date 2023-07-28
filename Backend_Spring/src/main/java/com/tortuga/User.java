9dbc697e12eb0f1e369bb61438670e17693902eb
BlasHernandez19 <133980150+BlasHernandez19@users.noreply.github.com>
2023-07-28 16:03:30 +0200

    Delete Backend_Spring directory

--- Backend_Spring/src/main/java/com/tortuga/User.java
+++
@@ -1,5 +0,0 @@
-package com.tortuga;
-
-public class User {
-
-}
--- Backend_Spring/src/main/java/com/tortuga/controllers/TortugaController.java
+++
@@ -1,7 +0,0 @@
-package com.tortuga.controllers;
-import org.springframework.boot.SpringApplication;
-public class TortugaController {
-
-@SpringBootApplication
-}
- 
