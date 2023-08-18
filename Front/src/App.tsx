56e3352c623d72d43aa7e1ef936632d7b5cbf043
Mordisquitos <mariojm2612@gmail.com>
2023-08-18 17:32:25 +0200

    midclass

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -6,9 +6,9 @@ import TaskCreator from "./TaskCreator";
 import { Task } from './types';
 import axios from 'axios'; 
 import DaysContainerLoader from "./DaysContainerLoader";
-import {
-  format
-} from 'date-fns';
+import { format } from 'date-fns';
+import { Button } from '@nextui-org/react'
+import EditIcon from "./IconComponents/EditIcon";
 
 const App: React.FC = () => {
 
@@ -19,14 +19,62 @@ const App: React.FC = () => {
   const handleNewTask = () => {
     setTaskCreatorVisible(!taskCreatorVisible);    
   };
+  const hardcodedTasks: Task[] = [
+    {
+      title: "Meeting with Team",
+      description: "Discuss project progress and updates with the team.",
+      beggining_hour: "10:00",
+      final_hour: "11:30",
+      duration: "1 hour 30 minutes",
+      category: "Meeting",
+      date: format(selectedDate, 'dd/MM/yyyy'),
+    },
+    {
+      title: "Write Blog Post",
+      description: "Write an informative blog post about recent industry trends.",
+      beggining_hour: "14:00",
+      final_hour: "16:00",
+      duration: "2 hours",
+      category: "Content Creation",
+      date: format(selectedDate, 'dd/MM/yyyy'),
+    },
+    {
+      title: "Gym Workout",
+      description: "Hit the gym for a cardio and strength training session.",
+      beggining_hour: "17:30",
+      final_hour: "18:30",
+      duration: "1 hour",
+      category: "Health & Fitness",
+      date: format(selectedDate, 'dd/MM/yyyy'),
+    },
+    {
+      title: "Read Book",
+      description: "Read chapters 5-8 of 'The Great Novel'",
+      beggining_hour: "20:00",
+      final_hour: "21:30",
+      duration: "1 hour 30 minutes",
+      category: "Personal Development",
+      date: format(selectedDate, 'dd/MM/yyyy'),
+    },
+    {
+      title: "Cook Dinner",
+      description: "Prepare a healthy and delicious dinner.",
+      beggining_hour: "19:00",
+      final_hour: "20:00",
+      duration: "1 hour",
+      category: "Cooking",
+      date: format(selectedDate, 'dd/MM/yyyy'),
+    },
+  ];
 
   useEffect(() => {
-    axios.get(`/api/tasks?date=${format(selectedDate, 'dd/MM/yyyy')}`)
+    axios.get(`/api/tasks?date=${format(selectedDate, 'yyyy-MM-dd')}`)
       .then(response => {
         setTasks(response.data);
       })
       .catch(error => {
         console.error('Error fetching tasks:', error);
+        setTasks(hardcodedTasks)
       });
   }, []);
 
@@ -39,12 +87,7 @@ const App: React.FC = () => {
       ) : (
         <DaysContainerLoader selectedDate={selectedDate} />
       )}
-      <div
-        className="newTaskButton"
-        onClick={handleNewTask}
-      >
-        <img src="src/assets/plus.png" alt="Nueva Tarea" />
-      </div>
+      <Button radius="full" startContent={<EditIcon/>} size="lg" isIconOnly className="fixed newTaskButton" color="primary" onPress={handleNewTask}></Button>
       {taskCreatorVisible && (
         <>
           <div className="fader" onClick={handleNewTask}></div>
--- Front/src/HeaderMP.tsx
+++ Front/src/HeaderMP.tsx
@@ -28,12 +28,10 @@ const HeaderMP = ({onThemeChange}:ThemeProps) => {
       <Link to="home" spy={true} smooth={true} duration={500} className="cursor-pointer">Home</Link>
       </NavbarItem>
       <NavbarItem>
-      <Link to="team" spy={true} smooth={true} duration={500} className="cursor-pointer">Team</Link>
+      <Link to="info" spy={true} smooth={true} duration={500} className="cursor-pointer">Info</Link>
       </NavbarItem>
       <NavbarItem>
-        <Link to="">
-          Customers
-        </Link>
+      <Link to="team" spy={true} smooth={true} duration={500} className="cursor-pointer">Team</Link>
       </NavbarItem>
     </NavbarContent>
     <NavbarContent justify="end">
