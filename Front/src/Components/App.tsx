44a38c70c26934b4cb7bb46951ce45c45fbd73ec
Mario <mariojm2612@gmail.com>
2023-08-20 04:26:24 +0200

    sorted

--- Front/src/Components/App.tsx
+++ Front/src/Components/App.tsx
@@ -6,7 +6,7 @@ import '../Styles/App.css'
 import '../Styles/Style.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {NextUIProvider} from "@nextui-org/react";
-import Estadisticas from './Estadisticas';
+import Estadisticas from './Analytics';
 
 function App() {
 
