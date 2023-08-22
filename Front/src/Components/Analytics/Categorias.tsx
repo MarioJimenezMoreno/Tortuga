1315ec171b19a9f6612488deb7c5dbfaeea1f849
MunchQuim <qpinedasotes@gmail.com>
2023-08-22 20:08:22 +0200

    cambios de mario

--- Front/src/Components/Analytics/Categorias.tsx
+++ Front/src/Components/Analytics/Categorias.tsx
@@ -1,15 +1,14 @@
-/**/
 import { FC } from 'react';
 import { Card } from '@nextui-org/react';
 
-interface Task {
+interface TaskAnalytics {
   username: string;
   name: string;
   duration: number;
   color_code: string;
 }
 
-const RESPUESTA_2: Task[] = [
+const RESPUESTA_2: TaskAnalytics[] = [
 
   {
     username: "Quim",
@@ -50,7 +49,7 @@ const RESPUESTA_2: Task[] = [
 ];
 
 // Reducción del array RESPUESTA_2
-let resultado_2 = RESPUESTA_2.reduce(function (acumulador: Task[], elemento: Task) {
+let resultado = RESPUESTA_2.reduce(function (acumulador: TaskAnalytics[], elemento: TaskAnalytics) {
   let elementoExistente = acumulador.find(function (item) {
     return item.name === elemento.name;
   });
@@ -64,34 +63,34 @@ let resultado_2 = RESPUESTA_2.reduce(function (acumulador: Task[], elemento: Tas
   return acumulador;
 }, []);
 
-// Ordenar el array resultado_2
-function compararMayorMenor(a: Task, b: Task) {
+// Ordenar el array resultado
+function compararMayorMenor(a: TaskAnalytics, b: TaskAnalytics) {
   return b.duration - a.duration;
 }
-resultado_2.sort(compararMayorMenor);
+resultado.sort(compararMayorMenor);
 
 const Categories: FC = () => {
   return (
     <div className="flex justify-between items-center">
-      <Card className="flex-grow p-4 text-black text-center" id="categoria_0" style={{ backgroundColor: resultado_2[0].color_code }}>
-        {resultado_2[0].name}:
+      <Card className="flex-grow p-4 text-black text-center" id="categoria_0" style={{ backgroundColor: resultado[0].color_code }}>
+        {resultado[0].name}:
         <br />
-        {resultado_2[0].duration} horas
+        {resultado[0].duration} horas
       </Card>
-      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_1" style={{ backgroundColor: resultado_2[1].color_code }}>
-        {resultado_2[1].name}:
+      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_1" style={{ backgroundColor: resultado[1].color_code }}>
+        {resultado[1].name}:
         <br />
-        {resultado_2[1].duration} horas
+        {resultado[1].duration} horas
       </Card>
-      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_2" style={{ backgroundColor: resultado_2[2].color_code }}>
-        {resultado_2[2].name}:
+      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_2" style={{ backgroundColor: resultado[2].color_code }}>
+        {resultado[2].name}:
         <br />
-        {resultado_2[2].duration} horas
+        {resultado[2].duration} horas
       </Card>
-      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_3" style={{ backgroundColor: resultado_2[3].color_code }}>
-        {resultado_2[3].name}:
+      <Card className="flex-grow p-4 ml-4 text-black text-center" id="categoria_3" style={{ backgroundColor: resultado[3].color_code }}>
+        {resultado[3].name}:
         <br />
-        {resultado_2[3].duration} horas
+        {resultado[3].duration} horas
       </Card>
     </div>
   );
--- Front/src/Components/Analytics/GraficoPastel.tsx
+++ Front/src/Components/Analytics/GraficoPastel.tsx
@@ -1,20 +1,85 @@
 
 import { Doughnut } from 'react-chartjs-2';
-import { ArcElement, Tooltip, Legend, Chart, CategoryScale, LinearScale } from 'chart.js';
+import { ArcElement, Tooltip, Legend, Chart } from 'chart.js';
 import axios from "axios";
+import { useEffect, useState } from 'react';
+import { ChartData, ChartOptions, DataElement } from '../../types';
 
 Chart.register(ArcElement, Tooltip, Legend);
 
-interface DataElement {
-  username: string;
-  name: string;
-  duration: number;
-  color_code: string;
-}
+const GraficoPastel = () => {
 
-let RESPUESTA_2: DataElement[] = []; 
+  const [tasks, setTasks] = useState<DataElement[]>([]);
+  const [colors, setColors] = useState<string[]>([]);
+  const [datasets, setDatasets] = useState<number[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
 
-axios
+  const hardcodedResponse = [{
+    username: "Quim",
+    name: "Libre",
+    duration: 10,
+    color_code: "#FCD8D4"
+  },
+  {
+    username: "Quim",
+    name: "Libre",
+    duration: 20,
+    color_code: "#FCD8D4"
+  },
+  {
+    username: "Quim",
+    name: "Trabajo",
+    duration: 36,
+    color_code: "#FDF6F0"
+  },
+  {
+    username: "Quim",
+    name: "Trabajo",
+    duration: 106,
+    color_code: "#FDF6F0"
+  },
+  {
+    username: "Quim",
+    name: "Ocio",
+    duration: 80,
+    color_code: "#F8E2CF"
+  },
+  {
+    username: "Quim",
+    name: "Deporte",
+    duration: 30,
+    color_code: "#F5C6AA"
+  }];
+  
+  function compararMayorMenor(a: DataElement, b: DataElement) {
+    return b.duration - a.duration;
+  }
+  
+  let resultado = tasks.reduce(function (acumulador: DataElement[], elemento) {
+    let elementoExistente = acumulador.find(function (item) {
+      return item.name === elemento.name;
+    });
+  
+    if (elementoExistente) {
+      elementoExistente.duration += elemento.duration;
+    } else {
+      acumulador.push(elemento);
+    }
+  
+    return acumulador;
+  }, []);
+
+  resultado.sort(compararMayorMenor);
+  
+  // Faltaría implementar setColors setDatasets setCategories
+  resultado.forEach(element => {
+    colors.push(element.color_code);
+    datasets.push(element.duration);
+    categories.push(element.name);
+  });
+
+  const fetchTasks = () => {
+  axios
 .get('http://localhost:8080/api/stats', {
   params: {
     // Aquí tus parámetros de consulta
@@ -24,142 +89,56 @@ axios
   .then(response => {
     // Manejar la respuesta del servidor
     console.log('Respuesta:', response.data);
-    RESPUESTA_2 = response.data;
+    setTasks(response.data);
   })
   .catch(error => {
     // Manejar errores
     console.error('Error:', error);
-    RESPUESTA_2 = [{
-      username: "Quim",
-      name: "Libre",
-      duration: 10,
-      color_code: "#FCD8D4"
-    },
-    {
-      username: "Quim",
-      name: "Libre",
-      duration: 20,
-      color_code: "#FCD8D4"
-    },
-    {
-      username: "Quim",
-      name: "Trabajo",
-      duration: 36,
-      color_code: "#FDF6F0"
-    },
-    {
-      username: "Quim",
-      name: "Trabajo",
-      duration: 106,
-      color_code: "#FDF6F0"
-    },
-    {
-      username: "Quim",
-      name: "Ocio",
-      duration: 80,
-      color_code: "#F8E2CF"
-    },
-    {
-      username: "Quim",
-      name: "Deporte",
-      duration: 30,
-      color_code: "#F5C6AA"
-    }];
+    setTasks(hardcodedResponse)
+    console.log("Hardcoded: "+ tasks)
   });
-
-  
-
-// para el grafico de PASTEL Y CATEGORIA
-let resultado_2 = RESPUESTA_2.reduce(function (acumulador: DataElement[], elemento) {
-  let elementoExistente = acumulador.find(function (item) {
-    return item.name === elemento.name;
-  });
-
-  if (elementoExistente) {
-    elementoExistente.duration += elemento.duration;
-  } else {
-    acumulador.push(elemento);
-  }
-
-  return acumulador;
-}, []);
-
-/*********-reorganiza el array de mayor a menor-*********/
-function compararMayorMenor(a: DataElement, b: DataElement) {
-  return b.duration - a.duration;
-}
-resultado_2.sort(compararMayorMenor);
-
-// Datos para el gráfico
-let color: string[] = [];
-let dataset: number[] = [];
-let categories: string[] = [];
-resultado_2.forEach(element => {
-  color.push(element.color_code);
-  dataset.push(element.duration);
-  categories.push(element.name);
-});
-
-export interface ChartData {
-  labels: string[];
-  datasets: {
-    label: string;
-    data: number[];
-    backgroundColor: string[];
-    borderColor: string;
-    borderWidth: number;
-  }[];
-}
-
-export interface ChartOptions {
-  scales: {
-    y: LinearScale[];
-  };
-  responsive: boolean;
-  maintainAspectRatio: boolean;
-  plugins: {
-    legend: {
-      position: string;
-      labels: {
-        color: string;
-      };
-    };
-  };
-}
-
-export const data: ChartData = {
-  labels: categories,
-  datasets: [
-    {
-      label: 'Label',
-      data: dataset,
-      backgroundColor: color,
-      borderColor: "#1B262C",
-      borderWidth: 1,
-    },
-  ],
 };
 
-export const options: ChartOptions = {
-  scales: {
-    y: [{
-      beginAtZero: true
-    }],
-  },
-  responsive: true,
-  maintainAspectRatio: true,
-  plugins: {
-    legend: {
-      position: "right",
-      labels: {
-        color: "#fff",
+  const data: ChartData = {
+    labels: categories,
+    datasets: [
+      {
+        label: 'Label',
+        data: datasets,
+        backgroundColor: colors,
+        borderColor: "#1B262C",
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const options: ChartOptions = {
+    scales: {
+      y: [{
+        beginAtZero: true // no existe pero si funciona ok
+      }],
+    },
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      legend: {
+        position: "right",
+        labels: {
+          color: "#fff",
+        },
       },
     },
-  },
-};
+  };
 
-function GraficoPastel() {
-  return <Doughnut data={data} options={options} height={340} />;
+  useEffect(() => {
+    setTasks([]);
+    fetchTasks();
+  }, []);
+
+  
+  return(
+  <Doughnut data={data} options={options} height={340} />
+  )
 }
 
 export default GraficoPastel;
