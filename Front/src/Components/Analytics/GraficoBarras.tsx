9696ceb804a5c823a94e5d3b6eac8687c296be32
Mario <mariojm2612@gmail.com>
2023-08-22 22:44:03 +0200

    stats

--- Front/src/Components/Analytics/GraficoBarras.tsx
+++ Front/src/Components/Analytics/GraficoBarras.tsx
@@ -1,4 +1,333 @@
+// //------------------------------------//
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+// import { Bar } from "react-chartjs-2";
+
+// // Definir la interfaz para los elementos en RESPUESTA_1
+// interface RespuestaItem {
+//   username: string;
+//   date: number;
+//   name: string;
+//   duration: number;
+//   color_code: string;
+// }
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// const RESPUESTA_1: RespuestaItem[] = [
+//   {
+//     username: "Quim",
+//     date: -1,
+//     name: "Libre",
+//     duration: 6,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -1,
+//     name: "Trabajo",
+//     duration: 1,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -1,
+//     name: "Ocio",
+//     duration: 14,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -1,
+//     name: "Ocio",
+//     duration: 12,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -1,
+//     name: "Deporte",
+//     duration: 5,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -2,
+//     name: "Libre",
+//     duration: 7,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -2,
+//     name: "Trabajo",
+//     duration: 3,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -2,
+//     name: "Ocio",
+//     duration: 15,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -2,
+//     name: "Deporte",
+//     duration: 4,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -3,
+//     name: "Libre",
+//     duration: 8,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -3,
+//     name: "Trabajo",
+//     duration: 5,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -3,
+//     name: "Ocio",
+//     duration: 10,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -3,
+//     name: "Deporte",
+//     duration: 3,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -4,
+//     name: "Libre",
+//     duration: 5,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -4,
+//     name: "Trabajo",
+//     duration: 6,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -4,
+//     name: "Ocio",
+//     duration: 9,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -4,
+//     name: "Deporte",
+//     duration: 6,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -5,
+//     name: "Libre",
+//     duration: 9,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -5,
+//     name: "Trabajo",
+//     duration: 7,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -5,
+//     name: "Ocio",
+//     duration: 11,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -5,
+//     name: "Deporte",
+//     duration: 2,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -6,
+//     name: "Libre",
+//     duration: 3,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -6,
+//     name: "Trabajo",
+//     duration: 8,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -6,
+//     name: "Ocio",
+//     duration: 8,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -6,
+//     name: "Deporte",
+//     duration: 7,
+//     color_code: "#66aaf9",
+//   },
+//   {
+//     username: "Quim",
+//     date: -7,
+//     name: "Libre",
+//     duration: 2,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     date: -7,
+//     name: "Trabajo",
+//     duration: 6,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     date: -7,
+//     name: "Ocio",
+//     duration: 15,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     date: -7,
+//     name: "Deporte",
+//     duration: 3,
+//     color_code: "#66aaf9",
+//   },
+// ];
+
+// //------------------------------------//
+
+// // para el grafico de BARRAS
+// let resultado: RespuestaItem[] = RESPUESTA_1.reduce(function (
+//   acumulador,
+//   elemento
+// ) {
+//   let elementoExistente = acumulador.find(function (item) {
+//     return item.date === elemento.date && item.name === elemento.name;
+//   });
+
+//   if (elementoExistente) {
+//     elementoExistente.duration += elemento.duration;
+//   } else {
+//     acumulador.push(elemento);
+//   }
+
+//   return acumulador;
+// },
+// []);
+
+// //------------------------------------//
+
+// // Obtener un objeto con días únicos
+// const diasUnicos: number[] = [...new Set(resultado.map((item) => item.date))];
+
+// // Preparar un objeto para los datos del gráfico
+// export const data = {
+//   labels: diasUnicos.map((date) => `Día ${date}`),
+//   datasets: resultado.reduce((datasets, item) => {
+//     const datasetIndex = datasets.findIndex((ds) => ds.label === item.name);
+//     if (datasetIndex !== -1) {
+//       datasets[datasetIndex].data.push(item.duration);
+//       datasets[datasetIndex].backgroundColor.push(item.color_code);
+//     } else {
+//       datasets.push({
+//         label: item.name,
+//         data: [item.duration],
+//         backgroundColor: [item.color_code],
+//       });
+//     }
+//     return datasets;
+//   }, [] as { label: string; data: number[]; backgroundColor: string[] }[]),
+// };
+
+// // Configuración del gráfico
+// export const options = {
+//   responsive: true,
+//   maintainAspectRatio: false,
+//   plugins: {
+//     legend: {
+//       labels: {
+//         color: undefined,
+//       },
+//     },
+//   },
+//   scales: {
+//     x: {
+//       stacked: true,
+//       ticks: {
+//         color: undefined, // Cambiar color del eje Y
+//       },
+//       grid: {
+//         /* color: "#000" // Cambiar color de la grilla */
+//       },
+//     },
+//     y: {
+//       stacked: true,
+//       beginAtZero: true,
+//       max: 24,
+
+//       ticks: {
+//         stepSize: 6,
+//         color: undefined, // Cambiar color del eje Y
+//       },
+//       grid: {
+//         /* color: "#000" // Cambiar color de la grilla */
+//       },
+//     },
+//   },
+// };
+
+// function GraficoBarras() {
+//   return (
+//     <div>
+//       <Bar options={options} data={data} height={340} />
+//     </div>
+//   );
+// }
+
+// export default GraficoBarras;
+
 //------------------------------------//
+import axios from "axios";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,16 +337,9 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-
-// Definir la interfaz para los elementos en RESPUESTA_1
-interface RespuestaItem {
-  username: string;
-  date: number;
-  name: string;
-  duration: number;
-  color_code: string;
-}
+import { DataElement, RespuestaItem } from "../../types";
 
 ChartJS.register(
   CategoryScale,
@@ -28,7 +350,7 @@ ChartJS.register(
   Legend
 );
 
-const RESPUESTA_1: RespuestaItem[] = [
+const hardcodedResponse = [
   {
     username: "Quim",
     date: -1,
@@ -234,89 +556,112 @@ const RESPUESTA_1: RespuestaItem[] = [
   },
 ];
 
-//------------------------------------//
-
-// para el grafico de BARRAS
-let resultado: RespuestaItem[] = RESPUESTA_1.reduce(function (
-  acumulador,
-  elemento
-) {
-  let elementoExistente = acumulador.find(function (item) {
-    return item.date === elemento.date && item.name === elemento.name;
-  });
-
-  if (elementoExistente) {
-    elementoExistente.duration += elemento.duration;
-  } else {
-    acumulador.push(elemento);
-  }
-
-  return acumulador;
-},
-[]);
-
-//------------------------------------//
-
-// Obtener un objeto con días únicos
-const diasUnicos: number[] = [...new Set(resultado.map((item) => item.date))];
-
-// Preparar un objeto para los datos del gráfico
-export const data = {
-  labels: diasUnicos.map((date) => `Día ${date}`),
-  datasets: resultado.reduce((datasets, item) => {
-    const datasetIndex = datasets.findIndex((ds) => ds.label === item.name);
-    if (datasetIndex !== -1) {
-      datasets[datasetIndex].data.push(item.duration);
-      datasets[datasetIndex].backgroundColor.push(item.color_code);
-    } else {
-      datasets.push({
-        label: item.name,
-        data: [item.duration],
-        backgroundColor: [item.color_code],
-      });
-    }
-    return datasets;
-  }, [] as { label: string; data: number[]; backgroundColor: string[] }[]),
-};
-
-// Configuración del gráfico
-export const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      labels: {
-        color: undefined,
-      },
-    },
-  },
-  scales: {
-    x: {
-      stacked: true,
-      ticks: {
-        color: undefined, // Cambiar color del eje Y
-      },
-      grid: {
-        /* color: "#000" // Cambiar color de la grilla */
-      },
-    },
-    y: {
-      stacked: true,
-      beginAtZero: true,
-      max: 24,
-
-      ticks: {
-        stepSize: 6,
-        color: undefined, // Cambiar color del eje Y
-      },
-      grid: {
-        /* color: "#000" // Cambiar color de la grilla */
-      },
-    },
-  },
-};
-
 function GraficoBarras() {
+  const [tasks, setTasks] = useState<DataElement[]>([]);
+  const fetchTasks = () => {
+    axios
+      .get("http://localhost:8080/api/stats", {
+        params: {
+          // Aquí tus parámetros de consulta
+          username: "QUIM", //MODIFICAR
+        },
+      })
+      .then((response) => {
+        // Manejar la respuesta del servidor
+        console.log("Respuesta:", response.data);
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        // Manejar errores
+        console.error("Error:", error);
+        setTasks(hardcodedResponse);
+      });
+  };
+
+  let resultado: RespuestaItem[] = tasks.reduce(function (
+    acumulador,
+    elemento
+  ) {
+    let elementoExistente = acumulador.find(function (item) {
+      return item.date === elemento.date && item.name === elemento.name;
+    });
+
+    if (elementoExistente) {
+      elementoExistente.duration += elemento.duration;
+    } else {
+      acumulador.push(elemento);
+    }
+
+    return acumulador;
+  },
+  []);
+
+  //------------------------------------//
+
+  // Obtener un objeto con días únicos
+  const diasUnicos: number[] = [...new Set(resultado.map((item) => item.date))];
+
+  // Preparar un objeto para los datos del gráfico
+  const data = {
+    labels: diasUnicos.map((date) => `Día ${date}`),
+    datasets: resultado.reduce((datasets, item) => {
+      const datasetIndex = datasets.findIndex((ds) => ds.label === item.name);
+      if (datasetIndex !== -1) {
+        datasets[datasetIndex].data.push(item.duration);
+        datasets[datasetIndex].backgroundColor.push(item.color_code);
+      } else {
+        datasets.push({
+          label: item.name,
+          data: [item.duration],
+          backgroundColor: [item.color_code],
+        });
+      }
+      return datasets;
+    }, [] as { label: string; data: number[]; backgroundColor: string[] }[]),
+  };
+
+  // Configuración del gráfico
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        labels: {
+          color: "#fff",
+        },
+      },
+    },
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          color: "#fff", // Cambiar color del eje Y
+        },
+        grid: {
+          /* color: "#000" // Cambiar color de la grilla */
+        },
+      },
+      y: {
+        stacked: true,
+        beginAtZero: true,
+        max: 24,
+
+        ticks: {
+          stepSize: 6,
+          color: "#fff", // Cambiar color del eje Y
+        },
+        grid: {
+          /* color: "#000" // Cambiar color de la grilla */
+        },
+      },
+    },
+  };
+
+  useEffect(() => {
+    setTasks([]);
+    fetchTasks();
+  }, []);
+
   return (
     <div>
       <Bar options={options} data={data} height={340} />
--- Front/src/Components/Analytics/GraficoPastel.tsx
+++ Front/src/Components/Analytics/GraficoPastel.tsx
@@ -1,157 +1,301 @@
+// import { Doughnut } from "react-chartjs-2";
+// import {
+//   ArcElement,
+//   Tooltip,
+//   Legend,
+//   Chart,
+//   CategoryScale,
+//   LinearScale,
+// } from "chart.js";
+
+// Chart.register(ArcElement, Tooltip, Legend);
+
+// interface DataElement {
+//   username: string;
+//   name: string;
+//   duration: number;
+//   color_code: string;
+// }
+// const RESPUESTA_2 = [
+//   {
+//     username: "Quim",
+//     name: "Libre",
+//     duration: 10,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     name: "Libre",
+//     duration: 20,
+//     color_code: "#74dfa2",
+//   },
+//   {
+//     username: "Quim",
+//     name: "Trabajo",
+//     duration: 36,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     name: "Trabajo",
+//     duration: 106,
+//     color_code: "#f9c97c",
+//   },
+//   {
+//     username: "Quim",
+//     name: "Ocio",
+//     duration: 80,
+//     color_code: "#f871a0",
+//   },
+//   {
+//     username: "Quim",
+//     name: "Deporte",
+//     duration: 30,
+//     color_code: "#66aaf9",
+//   },
+// ];
+
+// // para el grafico de PASTEL Y CATEGORIA
+// let resultado_2 = RESPUESTA_2.reduce(function (
+//   acumulador: DataElement[],
+//   elemento
+// ) {
+//   let elementoExistente = acumulador.find(function (item) {
+//     return item.name === elemento.name;
+//   });
+
+//   if (elementoExistente) {
+//     elementoExistente.duration += elemento.duration;
+//   } else {
+//     acumulador.push(elemento);
+//   }
+
+//   return acumulador;
+// },
+// []);
+
+// /*********-reorganiza el array de mayor a menor-*********/
+// function compararMayorMenor(a: DataElement, b: DataElement) {
+//   return b.duration - a.duration;
+// }
+// resultado_2.sort(compararMayorMenor);
+
+// // Datos para el gráfico
+// let color: string[] = [];
+// let dataset: number[] = [];
+// let categories: string[] = [];
+// resultado_2.forEach((element) => {
+//   color.push(element.color_code);
+//   dataset.push(element.duration);
+//   categories.push(element.name);
+// });
+
+// export interface ChartData {
+//   labels: string[];
+//   datasets: {
+//     label: string;
+//     data: number[];
+//     backgroundColor: string[];
+//     borderColor: string;
+//     borderWidth: number;
+//   }[];
+// }
+
+// export interface ChartOptions {
+//   scales: {
+//     y: LinearScale[];
+//   };
+//   responsive: boolean;
+//   maintainAspectRatio: boolean;
+//   plugins: {
+//     legend: {
+//       position: string;
+//       labels: {
+//         color: string;
+//       };
+//     };
+//   };
+// }
+
+// export const data: ChartData = {
+//   labels: categories,
+//   datasets: [
+//     {
+//       label: "Label",
+//       data: dataset,
+//       backgroundColor: color,
+//       borderColor: "#1B262C",
+//       borderWidth: 1,
+//     },
+//   ],
+// };
+
+// export const options: ChartOptions = {
+//   scales: {
+//     y: [
+//       {
+//         beginAtZero: true,
+//       },
+//     ],
+//   },
+//   responsive: true,
+//   maintainAspectRatio: true,
+//   plugins: {
+//     legend: {
+//       position: "right",
+//       labels: {
+//         color: undefined,
+//       },
+//     },
+//   },
+// };
+
+// function GraficoPastel() {
+//   return <Doughnut data={data} options={options} height={340} />;
+// }
+
+// export default GraficoPastel;
+
 import { Doughnut } from "react-chartjs-2";
-import {
-  ArcElement,
-  Tooltip,
-  Legend,
-  Chart,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
+import { ArcElement, Tooltip, Legend, Chart } from "chart.js";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ChartData, ChartOptions, DataElement } from "../../types";
 
 Chart.register(ArcElement, Tooltip, Legend);
 
-interface DataElement {
-  username: string;
-  name: string;
-  duration: number;
-  color_code: string;
-}
-const RESPUESTA_2 = [
-  {
-    username: "Quim",
-    name: "Libre",
-    duration: 10,
-    color_code: "#74dfa2",
-  },
-  {
-    username: "Quim",
-    name: "Libre",
-    duration: 20,
-    color_code: "#74dfa2",
-  },
-  {
-    username: "Quim",
-    name: "Trabajo",
-    duration: 36,
-    color_code: "#f9c97c",
-  },
-  {
-    username: "Quim",
-    name: "Trabajo",
-    duration: 106,
-    color_code: "#f9c97c",
-  },
-  {
-    username: "Quim",
-    name: "Ocio",
-    duration: 80,
-    color_code: "#f871a0",
-  },
-  {
-    username: "Quim",
-    name: "Deporte",
-    duration: 30,
-    color_code: "#66aaf9",
-  },
-];
+const GraficoPastel = () => {
+  const [tasks, setTasks] = useState<DataElement[]>([]);
+  const [colors, setColors] = useState<string[]>([]);
+  const [datasets, setDatasets] = useState<number[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
 
-// para el grafico de PASTEL Y CATEGORIA
-let resultado_2 = RESPUESTA_2.reduce(function (
-  acumulador: DataElement[],
-  elemento
-) {
-  let elementoExistente = acumulador.find(function (item) {
-    return item.name === elemento.name;
-  });
+  const hardcodedResponse = [
+    {
+      username: "Quim",
+      name: "Libre",
+      duration: 10,
+      color_code: "#74dfa2",
+    },
+    {
+      username: "Quim",
+      name: "Libre",
+      duration: 20,
+      color_code: "#74dfa2",
+    },
+    {
+      username: "Quim",
+      name: "Trabajo",
+      duration: 36,
+      color_code: "#f9c97c",
+    },
+    {
+      username: "Quim",
+      name: "Trabajo",
+      duration: 106,
+      color_code: "#f9c97c",
+    },
+    {
+      username: "Quim",
+      name: "Ocio",
+      duration: 80,
+      color_code: "#f871a0",
+    },
+    {
+      username: "Quim",
+      name: "Deporte",
+      duration: 30,
+      color_code: "#66aaf9",
+    },
+  ];
 
-  if (elementoExistente) {
-    elementoExistente.duration += elemento.duration;
-  } else {
-    acumulador.push(elemento);
+  function compararMayorMenor(a: DataElement, b: DataElement) {
+    return b.duration - a.duration;
   }
 
-  return acumulador;
-},
-[]);
+  let resultado = tasks.reduce(function (acumulador: DataElement[], elemento) {
+    let elementoExistente = acumulador.find(function (item) {
+      return item.name === elemento.name;
+    });
 
-/*********-reorganiza el array de mayor a menor-*********/
-function compararMayorMenor(a: DataElement, b: DataElement) {
-  return b.duration - a.duration;
-}
-resultado_2.sort(compararMayorMenor);
+    if (elementoExistente) {
+      elementoExistente.duration += elemento.duration;
+    } else {
+      acumulador.push(elemento);
+    }
 
-// Datos para el gráfico
-let color: string[] = [];
-let dataset: number[] = [];
-let categories: string[] = [];
-resultado_2.forEach((element) => {
-  color.push(element.color_code);
-  dataset.push(element.duration);
-  categories.push(element.name);
-});
+    return acumulador;
+  }, []);
 
-export interface ChartData {
-  labels: string[];
-  datasets: {
-    label: string;
-    data: number[];
-    backgroundColor: string[];
-    borderColor: string;
-    borderWidth: number;
-  }[];
-}
+  resultado.sort(compararMayorMenor);
 
-export interface ChartOptions {
-  scales: {
-    y: LinearScale[];
+  // Faltaría implementar setColors setDatasets setCategories
+  resultado.forEach((element) => {
+    colors.push(element.color_code);
+    datasets.push(element.duration);
+    categories.push(element.name);
+  });
+
+  const fetchTasks = () => {
+    axios
+      .get("http://localhost:8080/api/stats", {
+        params: {
+          // Aquí tus parámetros de consulta
+          username: "QUIM", //MODIFICAR
+        },
+      })
+      .then((response) => {
+        // Manejar la respuesta del servidor
+        console.log("Respuesta:", response.data);
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        // Manejar errores
+        console.error("Error:", error);
+        setTasks(hardcodedResponse);
+        console.log("Hardcoded: " + tasks);
+      });
   };
-  responsive: boolean;
-  maintainAspectRatio: boolean;
-  plugins: {
-    legend: {
-      position: string;
-      labels: {
-        color: string;
-      };
-    };
-  };
-}
 
-export const data: ChartData = {
-  labels: categories,
-  datasets: [
-    {
-      label: "Label",
-      data: dataset,
-      backgroundColor: color,
-      borderColor: "#1B262C",
-      borderWidth: 1,
-    },
-  ],
-};
-
-export const options: ChartOptions = {
-  scales: {
-    y: [
+  const data: ChartData = {
+    labels: categories,
+    datasets: [
       {
-        beginAtZero: true,
+        label: "Label",
+        data: datasets,
+        backgroundColor: colors,
+        borderColor: "#1B262C",
+        borderWidth: 1,
       },
     ],
-  },
-  responsive: true,
-  maintainAspectRatio: true,
-  plugins: {
-    legend: {
-      position: "right",
-      labels: {
-        color: undefined,
+  };
+
+  const options: ChartOptions = {
+    scales: {
+      y: [
+        {
+          beginAtZero: true, // no existe pero si funciona ok
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      legend: {
+        position: "right",
+        labels: {
+          color: "#fff",
+        },
       },
     },
-  },
-};
+  };
 
-function GraficoPastel() {
+  useEffect(() => {
+    setTasks([]);
+    fetchTasks();
+  }, []);
+
   return <Doughnut data={data} options={options} height={340} />;
-}
+};
 
 export default GraficoPastel;
